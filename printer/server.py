15c51305afd6630950a7f4719d786efc3b4ac398
John Schulz <jr.john.schulz@gmail.com>
2020-01-25 01:20:08 -0500

    PrinterPi API Routes
    test, enable, disable, get-status

--- printer/server.py
+++ printer/server.py
@@ -1,6 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+
+@app.route("/test/<printer_id>")
+def run_test(printer_id):
     return "Hello World"
+
+
+@app.route("/disable/<printer_id>")
+def disable_printer(printer_id):
+    return 0
+
+
+@app.route("/enable/<printer_id>")
+def enable_printer(printer_id):
+    return 0
+
+
+@app.route("/get-status/<printer_id>")
+def take_photo(printer_id):
+    return 0
