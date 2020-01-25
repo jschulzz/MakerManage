a79acd8958ace9e980fe991a28b42f09bb2ab9e6
John Schulz <jr.john.schulz@gmail.com>
2020-01-25 14:57:30 -0500

    Added Trello Webhook Filter
    This filter looks for Off limits labels being applied cards, and prints to the terminal the name of the printer and states that it is off limits.
    
    Also the custom fields on the trello cards were introduced in this commit.

--- controller/server.py
+++ controller/server.py
@@ -3,15 +3,25 @@ from flask import request
 import json
 
 app = Flask(__name__)
+app.config["ENV"] = "development"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 @app.route("/trelloCallback", methods=["GET", "POST"])
-def login():
+def callback():
     if request.method == "POST":
         res = request.json
         change = res.get("action")
         change_type = change.get("type")
+        change_label = change.get("display").get("entities").get("label")
+        change_card = change.get("display").get("entities").get("card")
         # print(res.get("action").get("type"))
         if change_type == "addLabelToCard":
             print(json.dumps(change, indent=2))
+            if change_label.get("text") == "Off Limits":
+                print(change_card.get("text") + " is off limits")
+
+
     return "CallingBack"
