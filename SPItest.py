f3a147f0a471fcfa7853780cb0cf722dc69102b7
Evan King <evantheking01@gmail.com>
2020-01-25 08:31:39 -0500

    go
    cats

--- SPItest.py
+++ SPItest.py
@@ -6,6 +6,7 @@ from mfrc522 import MFRC522
 import mfrc522
 CommandReg = 0x01
 reader = SimpleMFRC522()
+reader.READER.Write_MFRC522(CommandReg,0x2F)
 print(reader.READER.Read_MFRC522(CommandReg))
 
 GPIO.cleanup()
