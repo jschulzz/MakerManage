f97d79f594bd366e689c20fe3d76503bb6e26748
John Schulz <jr.john.schulz@gmail.com>
2020-01-25 01:30:18 -0500

    Adding Types to routes

--- printer/server.py
+++ printer/server.py
@@ -3,21 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/test/<printer_id>")
+@app.route("/test/<int:printer_id>")
 def run_test(printer_id):
     return "Hello World"
 
 
-@app.route("/disable/<printer_id>")
+@app.route("/disable/<int:printer_id>")
 def disable_printer(printer_id):
     return "disable %i" % (printer_id)
 
 
-@app.route("/enable/<printer_id>")
+@app.route("/enable/<int:printer_id>")
 def enable_printer(printer_id):
     return "enable %i" % (printer_id)
 
 
-@app.route("/get-status/<printer_id>")
+@app.route("/get-status/<int:printer_id>")
 def get_status(printer_id):
     return "check on %i" % (printer_id)
