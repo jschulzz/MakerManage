8bc546de7c12a793cb764369dd983efc9d11741e
Evan King <evantheking01@gmail.com>
2020-01-25 06:10:06 -0500

    dont look at me
    im flailing

--- SPItest.py
+++ SPItest.py
@@ -2,7 +2,8 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-
+from mfrc522 import MFRC522
+import mfrc522
 reader = SimpleMFRC522()
 print(reader.READER.Read_MFRC522(CommandReg))
 
